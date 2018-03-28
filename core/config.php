5cada538e075132051e1d2e9f353a5cf06e86a50
Wing Chiu Choi <docm1234.wc@gmail.com>
2018-03-28 23:00:29 +0800

    add config

--- core/config.php
+++ core/config.php
@@ -3,5 +3,7 @@ return [
     'config' => [
         'displayErrorDetails' => true,
         'addContentLengthHeader' => true,
+        'ethereumNode' => 'http://35.229.228.204:8545',
+        'bitcoinNode' => 'http://35.201.209.252:8332'
     ],
 ];
