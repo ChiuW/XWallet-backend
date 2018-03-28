2412a14a88a038e5bfaf6b8273f68644d26ffeab
Wing Chiu Choi <docm1234.wc@gmail.com>
2018-03-28 23:25:36 +0800

    update config

--- core/config.php
+++ core/config.php
@@ -2,8 +2,6 @@
 return [
     'config' => [
         'displayErrorDetails' => true,
-        'addContentLengthHeader' => true,
-        'ethereumNode' => 'http://35.229.228.204:8545',
-        'bitcoinNode' => 'http://35.201.209.252:8332'
+        'addContentLengthHeader' => true
     ],
 ];
